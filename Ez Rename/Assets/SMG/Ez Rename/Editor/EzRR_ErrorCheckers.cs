02756ccd53ccddb33c602eaa49880bbcf02a9baf
Beto de Oliveira <betodeoliveira@outlook.com>
2019-05-09 15:01:42 -0300

    First commity

---
+++ Ez Rename/Assets/SMG/Ez Rename/Editor/EzRR_ErrorCheckers.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using System.IO;
+
+namespace SMG.EzRenamer
+{
+	public class EzRR_ErrorCheckers : MonoBehaviour 
+	{
+		public static bool CheckForDifferentParents(GameObject[] gameObjectSelection)
+		{
+			List<Transform> _parents = new List<Transform>();
+			for (int i = 0; i < gameObjectSelection.Length; i++)
+			{
+				if (!_parents.Contains(gameObjectSelection[i].transform.parent))
+					_parents.Add(gameObjectSelection[i].transform.parent);
+
+				if (_parents.Count > 1)
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public static bool CheckForDifferentTypes(Object[] objectsSelected)
+		{
+			List<string> _types = new List<string>();
+			for (int i = 0; i < objectsSelected.Length; i++)
+			{
+				if (!_types.Contains(Path.GetExtension(AssetDatabase.GetAssetPath(objectsSelected[i])).ToString()))
+					_types.Add(Path.GetExtension(AssetDatabase.GetAssetPath(objectsSelected[i])).ToString());
+
+				if (_types.Count > 1)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		public static bool CheckForBlockedExtension(Object[] objectsSelected)
+		{
+			string _extension = "";
+			_extension = Path.GetExtension(AssetDatabase.GetAssetPath(objectsSelected[0])).ToString();
+			if (string.Equals(_extension, ".cs") || string.Equals(_extension, ".js") || string.Equals(_extension, ".shader"))
+			{
+				return true;
+			}
+			return false;
+		}
+		
+		public static bool CheckForInvalidCharacters(string value)
+		{
+			// Verifies invalid characters
+			if(value.Contains("<") || 
+				value.Contains(">") || 
+				value.Contains(":") || 
+				value.Contains('"') || 
+				value.Contains("/") || 
+				value.Contains("\\") || 
+				value.Contains("|") || 
+				value.Contains("?") || 
+				value.Contains("*"))
+			{
+				return true;
+			}
+			return false;
+		}
+		
+		public static bool CheckForInvalidNameEndings(string value)
+		{
+			if(value.EndsWith(" ") || value.EndsWith("."))
+			{
+				return true;
+			}
+			return false;
+		}
+		
+		public static bool CheckForStringNullOrEmpty(string value)
+		{
+			if(string.IsNullOrEmpty(value))
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Ez Rename/Assets/SMG/Ez Rename/Editor/EzRR_Style.cs
@@ -0,0 +1,94 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SMG.EzRenamer
+{
+    public class EzRR_Style : Editor
+    {
+        public const int smallBtnHeight = 15;
+        public const int mediumBtnHeight = 23;
+        public const int bigBtnHeight = 30;
+        public static GUIStyle customStyle = new GUIStyle();
+        public static Color32 uiLineColor = new Color32(89, 89, 89, 255);
+        public delegate void ButtonAction();
+
+        public static void DrawHeader(string title)
+        {
+            // Configurate the style to mach the foldout one
+            customStyle = new GUIStyle(EditorStyles.label);
+            customStyle.fontStyle = FontStyle.Bold;
+            // Draw the header
+            EditorGUILayout.LabelField(title, customStyle);
+            // Revert the style
+            customStyle = EditorStyles.label;
+        }
+
+        public static bool DrawFoldoutHeader(string title, bool show)
+        {
+            // Configurate the style to mach the foldout one
+            customStyle = new GUIStyle(EditorStyles.foldout);
+            customStyle.fontStyle = FontStyle.Bold;
+            // Draw the header
+            show = EditorGUILayout.Foldout(show, title, customStyle);
+            customStyle = EditorStyles.foldout;
+            return show;
+        }
+
+        public static void DrawUILine(Color color, int thickness = 1, int padding = 10)
+        {
+            Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+            r.height = thickness;
+            r.y += padding / 2;
+            r.x -= 2;
+            r.width += 6;
+            EditorGUI.DrawRect(r, color);
+        }
+
+        public static void DrawColoredLabelField(Color color, string text)
+        {
+            customStyle = new GUIStyle(EditorStyles.label);
+            customStyle.normal.textColor = color;
+            EditorGUILayout.LabelField(text, customStyle);
+        }
+
+        public static bool ToggleLeftBold(string title, bool value)
+        {
+            customStyle = new GUIStyle(EditorStyles.label);
+            customStyle.fontStyle = FontStyle.Bold;
+            value = EditorGUILayout.ToggleLeft(title, value, customStyle);
+            customStyle = EditorStyles.foldout;
+            return value;
+        }
+
+        public static bool ToggleLeftBold(string title, bool value, float customWidth)
+        {
+            customStyle = new GUIStyle(EditorStyles.label);
+            customStyle.fontStyle = FontStyle.Bold;
+            value = EditorGUILayout.ToggleLeft(title, value, customStyle, GUILayout.Width(customWidth));
+            customStyle = EditorStyles.foldout;
+            return value;
+        }
+
+        public static void DrawHelpButton(string url)
+        {
+            // Creates the custom style to remove the button layout and leave just the texture
+            customStyle = new GUIStyle();
+            // Creates the button using the _Help texture from Unity
+            if (GUILayout.Button(EditorGUIUtility.FindTexture("_Help"), customStyle, GUILayout.Width(20), GUILayout.Height(20)))
+            {
+                Application.OpenURL(url);
+            }
+        }
+
+        public static void DrawFindButton(ButtonAction action)
+        {
+            // Creates the custom style to remove the button layout and leave just the texture
+            customStyle = new GUIStyle();
+            // Creates the button using the ShurikenObjectField texture from Unity
+            if (GUILayout.Button(EditorGUIUtility.IconContent("ShurikenObjectField"), customStyle, GUILayout.Width(30), GUILayout.Height(20)))
+            {
+                action();
+            }
+        }
+    }
+}
